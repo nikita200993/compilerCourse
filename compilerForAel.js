c05ee9e5f88c4696f260d71e6a8180445b65eb20
NikitaRR <romanyuk@smartdec.ru>
2020-05-16 20:10:09 +0300

    js

--- compilerForAel.js
+++ compilerForAel.js
@@ -1,12 +1,29 @@
 const Path = require("path");
+const Os = require("os");
 const aelGrammar = require(Path.resolve(__dirname, "lessonOne.js"));
 
+/*
+ Classes of ast nodes
+*/
 class Program
 {
 
     constructor(body)
     {
         this.body = body;
+    }
+
+    analyze()
+    {
+        const context = new Set();
+        this.body.forEach(stmt => stmt.analyze(context));
+        return this;
+    }
+
+    generateJavaScriptCode()
+    {
+        const declaredIds = new Set();
+        return this.body.map((value, index, array) => value.generateJavaScriptCode(declaredIds, "")).join(Os.EOL);
     }
 }
 
@@ -18,6 +35,21 @@ class Assignment
         this.id = id;
         this.expression = expression;
     }
+
+    analyze(context)
+    {
+        this.expression.analyze(context);
+        context.add(this.id);
+    }
+
+    generateJavaScriptCode(declaredIds, indent)
+    {
+        const prefix = declaredIds.has(this.id.name) ? "" : "var ";
+        declaredIds.add(this.id.name);
+        return `${indent}${prefix}_${this.id} = ${
+            this.expression.generateJavaScriptCode()
+            };`;
+    }
 }
 
 class Print
@@ -26,6 +58,16 @@ class Print
     constructor(expression)
     {
         this.expression = expression;
+    }
+
+    analyze(context)
+    {
+        this.expression.analyze(context);
+    }
+
+    generateJavaScriptCode(declaredIds, indent)
+    {
+        return `${indent}console.log(${this.expression.generateJavaScriptCode()});`;
     }
 }
 
@@ -37,6 +79,23 @@ class While
         this.condition = condition;
         this.body = body;
     }
+
+    analyze(context)
+    {
+        this.condition.analyze(context);
+        this.body.forEach(stmt => stmt.analyze(context));
+    }
+
+    generateJavaScriptCode(declaredIds, indent)
+    {
+        return `${indent}while (${this.condition.generateJavaScriptCode()} > 0) {`
+            + Os.EOL
+            + this.body.map(
+                (value, index, array) => value.generateJavaScriptCode(declaredIds, indent + "    ")
+            ).join(Os.EOL)
+            + Os.EOL
+            + `${indent}}`;
+    }
 }
 
 class BinaryExp
@@ -45,6 +104,17 @@ class BinaryExp
     constructor(left, op, right)
     {
         Object.assign(this, { left, op, right });
+    }
+
+    analyze(context)
+    {
+        this.left.analyze(context);
+        this.right.analyze(context);
+    }
+
+    generateJavaScriptCode()
+    {
+        return `(${this.left.generateJavaScriptCode()} ${this.op} ${this.right.generateJavaScriptCode()})`;
     }
 }
 
@@ -56,6 +126,16 @@ class UnaryExp
         this.op = op;
         this.operand = operand;
     }
+
+    analyze(context)
+    {
+        this.operand.analyze(context);
+    }
+
+    generateJavaScriptCode()
+    {
+        return `(${op}${this.operand.generateJavaScriptCode()})`;
+    }
 }
 
 class NumericalLiteral
@@ -64,6 +144,13 @@ class NumericalLiteral
     constructor(value)
     {
         this.value = value;
+    }
+
+    analyze(context) { };
+
+    generateJavaScriptCode()
+    {
+        return this.value;
     }
 }
 
@@ -74,58 +161,96 @@ class Identifier
     {
         this.name = name;
     }
+
+    analyze(context)
+    {
+        if (!context.has(this.name)) {
+            throw new Error(`Identifier ${this.name} has not been declared.`)
+        }
+    }
+
+    generateJavaScriptCode()
+    {
+        return "_" + this.name;
+    }
 }
 
+/*
+Creating semantics that creates ast.
+*/
 const astBuilder = aelGrammar.createSemantics()
     .addOperation(
         "ast",
         {
-            Program(body) {
-                return body.ast();
+            Program(body)
+            {
+                return new Program(body.ast());
             },
-            
-            Statement_assign(id, eq, expr, _semicolon) {
+
+            WhileStatement(_while, _openParen, exp, _closeParen, _openBrace, block, _closeBrace)
+            {
+                return new While(exp.ast(), block.ast());
+            },
+
+            Statement_assign(id, eq, expr, _semicolon)
+            {
                 return new Assignment(id.sourceString, expr.ast());
             },
 
-            Statement_print(_print, expr, _semicolon) {
+            Statement_print(_print, expr, _semicolon)
+            {
                 return new Print(expr.ast());
             },
 
-            Exp_plus(left, op, right) {
-                return new BinaryExp(left.ast(), op, right.ast());
+            Exp_plus(left, op, right)
+            {
+                return new BinaryExp(left.ast(), op.sourceString, right.ast());
             },
 
-            Exp_minus(left, op, right) {
-                return new BinaryExp(left.ast(), op, right.ast());
+            Exp_minus(left, op, right)
+            {
+                return new BinaryExp(left.ast(), op.sourceString, right.ast());
             },
 
-            Term_times(left, op, right) {
-                return new BinaryExp(left.ast(), op, right.ast());
+            Term_times(left, op, right)
+            {
+                return new BinaryExp(left.ast(), op.sourceString, right.ast());
             },
 
-            Term_divide(left, op, right) {
-                return new BinaryExp(left.ast(), op, right.ast());
+            Term_divide(left, op, right)
+            {
+                return new BinaryExp(left.ast(), op.sourceString, right.ast());
             },
 
-            PowerTerm_power(left, op, rigth) {
-                return BinaryExp(left.ast(), op, right.ast());
+            PowerTerm_power(left, op, right)
+            {
+                return new BinaryExp(left.ast(), op.sourceString, right.ast());
             },
 
-            Factor_negate(op, expr) {
-                return new UnaryExp(op, expr.ast());
+            Factor_negate(op, expr)
+            {
+                return new UnaryExp(op.sourceString, expr.ast());
             },
 
-            Primary_parens(_openParen, expr, _closeParen) {
-                return new expr.ast();
+            Primary_parens(_openParen, expr, _closeParen)
+            {
+                return expr.ast();
             },
 
-            number(_chars) {
+            number(_chars)
+            {
                 return new NumericalLiteral(+this.sourceString);
             },
 
-            id(_first, _rest) {
+            id(_first, _rest)
+            {
                 return new Identifier(this.sourceString);
             }
         }
     )
+
+
+/*
+    Exporting ast builder.
+*/
+module.exports = astBuilder;
--- test/aelCompilerTest.js
+++ test/aelCompilerTest.js
@@ -1,8 +1,10 @@
 const expect = require("chai").expect;
 const aelGrammar = require("../lessonOne.js")
-const interpreterModule = require("../interpreter.js");
+const interpreterModule = require("../interpreter");
+const astBuilder = require("../compilerForAel")
 const interpreter = interpreterModule.extendedInterpreter;
 const memory = interpreterModule.memory;
+
 
 describe(
     "aelInterpreter test.",
@@ -23,6 +25,33 @@ describe(
                 expect(match.succeeded()).to.be.true;
                 interpreter(match).exec();
                 expect(memory.get("j")).equal(2);
+            }
+        )
+    }
+)
+
+describe(
+    "aelCompilerTest",
+    function ()
+    {
+        it(
+            "checking ast building",
+            function ()
+            {
+                const match = aelGrammar.match(`
+                    i = 4;
+                    while (i) {
+                        print i;
+                        i = i - 1;
+                        a = i ** 2 * 3 + 4 * (5 + 6) * j;
+                        print a;
+                    }
+                `)
+                expect(match.succeeded()).to.be.true;
+                expect(() => astBuilder(match).ast().analyze()).to.Throw(
+                    Error,
+                    "Identifier j has not been declared."
+                )
             }
         )
     }
